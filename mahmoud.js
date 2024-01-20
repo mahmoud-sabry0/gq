7402f886d954650fd9a9a838ad58be8ddb65be9c
mahmoud-sabry0 <mahmoud.sabry.ahmed000@gmail.com>
2024-01-20 17:27:24 +0200

    Initial commit

---
+++ mahmoud.js
@@ -0,0 +1,39 @@
+$('#icon').on('click',function(){
+    
+    $('#mahmoud').animate({width:'toggle' ,paddingInline:'toggle'}  ,  1000)
+})
+
+
+$('.toggle ').on('click',function(){
+    
+    $('.inner').animate({height:'toggle' ,paddingInline:'toggle' }  ,  1000)
+    
+});
+
+
+let nums = 0;
+
+let idInterval = setInterval(function(){
+   
+
+    document.getElementById('seconds').innerHTML=nums;
+
+    if(nums == 100){
+        clearInterval(idInterval)
+    }
+    nums++;
+}, 1000);
+
+
+let minute = 0;
+
+let tim = setInterval(function(){
+   
+
+    document.getElementById('minutes').innerHTML=minute;
+
+    if(minute == 100){
+        clearInterval(idInterval)
+    }
+    minute++;
+}, 60000);
